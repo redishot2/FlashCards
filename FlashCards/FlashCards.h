2a1c687cd3d28c9396a18f3daf6ebed7132b26ff
Natasha-E-Martinez <natasha.e.martinez@disney.com>
2020-03-03 12:25:43 -0800

    First push

---
+++ FlashCards/FlashCards.h
@@ -0,0 +1,19 @@
+//
+//  FlashCards.h
+//  FlashCards
+//
+//  Created by Martinez, Natasha E. on 3/3/20.
+//  Copyright © 2020 Martinez, Natasha E. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlashCards.
+FOUNDATION_EXPORT double FlashCardsVersionNumber;
+
+//! Project version string for FlashCards.
+FOUNDATION_EXPORT const unsigned char FlashCardsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlashCards/PublicHeader.h>
+
+
